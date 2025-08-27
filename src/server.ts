233b3ffa4ee4c2166866dcb421524efe2639284a
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2025-08-27 19:45:16 +0200

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,21 @@
 import express from 'express' 
 import colors from 'colors'
 import morgan from 'morgan'
+import { db } from './config/db'
+
+async function connectDB() {
+    try {
+        await db.authenticate();
+        db.sync()  // This will create tables and syncronice the models with the database
+        console.log(colors.blue.bold('Database connected...'.cyan.underline));
+        
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+        
+    }
+}
+
+connectDB();
 
 const app = express()
 
