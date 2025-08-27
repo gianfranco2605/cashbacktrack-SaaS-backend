6f9e00eb669995013118674b1b7c65784e4af98d
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2025-08-27 19:41:41 +0200

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	new file:   db.ts
    
     Changes not staged for commit:
    	modified:   ../../package-lock.json
    	modified:   ../../package.json
    	modified:   ../server.ts

---
+++ src/config/db.ts
@@ -0,0 +1,15 @@
+import { Sequelize } from 'sequelize-typescript';
+import dotenv  from 'dotenv';
+
+dotenv.config();
+
+export const db = new Sequelize( process.env.DATABASE_URL,  {
+    
+    dialectOptions: {
+        ssl: {
+            require: false,
+            
+        }
+    }
+
+} )
