54bdfeaec9bb547e5e2260d4fafaad10c9ff4b2a
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2025-09-22 21:37:53 +0200

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   src/controllers/AuthController.ts
    	modified:   src/routes/authRouter.ts

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,10 +1,26 @@
 import type { Request, Response } from 'express';
+import User from '../models/User';
 
 export class AuthController {
 
     static createAccount = async (req: Request, res: Response) => {
 
-        res.json('Creating account')
-    }
+        const{ email } = req.body
 
+        const userExist = await User.findOne({where: {email}})
+
+        if(userExist) {
+            const error = new Error('User already exist')
+            return res.status(409).json({error: error.message})
+        }
+
+        try {
+            const user = new User(req.body)
+            await user.save();
+            res.json('Account created')
+        } catch (error) {
+            //console.log(error)
+            res.status(500).json({error: 'there was an error'})
+        }
+    }
 }
--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,8 +1,20 @@
 import { Router } from "express";
 import { AuthController } from '../controllers/AuthController'
+import { body } from "express-validator";
+import { handleInputErrors } from "../middleware/validation";
 
 const router = Router();
 
-router.post('/create-account', AuthController.createAccount);
+router.post('/create-account',
+    body('name')
+    .notEmpty().withMessage('Name cant be empty'),
+    body('password')
+    .notEmpty().withMessage('Paswword to short'),
+    body('email')
+    .notEmpty().withMessage('E-mail not valid'),
+    handleInputErrors,
+    AuthController.createAccount
+);
+
 
 export default router;
