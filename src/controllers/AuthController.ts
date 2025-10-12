9137ce8789f7f6b5a9bddb879327e3433ec69ebd
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2025-10-12 21:13:45 +0200

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   src/controllers/AuthController.ts
    	new file:   src/middleware/suth.ts
    	modified:   src/routes/authRouter.ts

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -4,18 +4,17 @@ import { checkPassword, hashPassword } from '../utils/auth';
 import { generateToken } from '../utils/token';
 import { AuthEmail } from '../emails/AuthEmail';
 import { generateJWT } from '../utils/jwt';
-
 export class AuthController {
 
     static createAccount = async (req: Request, res: Response) => {
 
-        const{ name, email, password } = req.body
+        const {  email, password } = req.body
 
-        const userExist = await User.findOne({where: {email}})
+        const userExist = await User.findOne({ where: { email } })
 
-        if(userExist) {
+        if (userExist) {
             const error = new Error('User already exist')
-            return res.status(409).json({error: error.message})
+            return res.status(409).json({ error: error.message })
         }
 
         try {
@@ -35,24 +34,24 @@ export class AuthController {
             } catch (emailError) {
                 console.log('Email sending failed, but user was created:', emailError.message);
             }
-            
+
             res.json('Account created');
 
         } catch (error) {
             console.log(error)
-            res.status(500).json({error: 'there was an error'})
+            res.status(500).json({ error: 'there was an error' })
         }
     }
 
     static confirmAccount = async (req: Request, res: Response) => {
 
-        const {token} = req.body;
-        const user = await User.findOne({where: {token}})
+        const { token } = req.body;
+        const user = await User.findOne({ where: { token } })
         console.log(user);
-        
-        if(!user) {
+
+        if (!user) {
             const error = new Error('Token not valid');
-            return res.status(401).json({error: error.message})
+            return res.status(401).json({ error: error.message })
         }
 
         user.confirmed = true;
@@ -65,42 +64,42 @@ export class AuthController {
 
     static login = async (req: Request, res: Response) => {
 
-        const{ email, password } = req.body
+        const { email, password } = req.body
 
-        const user = await User.findOne({where: {email}})
+        const user = await User.findOne({ where: { email } })
 
-        if(!user) {
+        if (!user) {
             const error = new Error('User not found')
-            return res.status(404).json({error: error.message})
+            return res.status(404).json({ error: error.message })
         }
 
-        if(!user.confirmed) {
+        if (!user.confirmed) {
             const error = new Error('Account not confirmed')
-            return res.status(403).json({error: error.message})
+            return res.status(403).json({ error: error.message })
         }
 
         const isPasswrodcorrect = await checkPassword(password, user.password)
 
-        if(!isPasswrodcorrect) {
+        if (!isPasswrodcorrect) {
             const error = new Error('Password is incorrect')
-            return res.status(401).json({error: error.message})
+            return res.status(401).json({ error: error.message })
         }
 
-        const token = generateJWT(user.id)  
-        
+        const token = generateJWT(user.id)
+
         res.json(token)
-        
+
     }
 
     static forgotPassword = async (req: Request, res: Response) => {
 
-        const{ email } = req.body
+        const { email } = req.body
 
-        const user = await User.findOne({where: {email}})
+        const user = await User.findOne({ where: { email } })
 
-        if(!user) {
+        if (!user) {
             const error = new Error('User not found')
-            return res.status(404).json({error: error.message})
+            return res.status(404).json({ error: error.message })
         }
 
         user.token = generateToken();
@@ -114,38 +113,38 @@ export class AuthController {
         })
 
         res.json('Check you email for a link to reset your password')
-        
+
     }
 
     static validateToken = async (req: Request, res: Response) => {
 
-        const{ token } = req.body
+        const { token } = req.body
 
         console.log(token);
 
-        const tokenExist = await User.findOne({where: {token}})
+        const tokenExist = await User.findOne({ where: { token } })
 
-        if(!tokenExist) {
+        if (!tokenExist) {
             const error = new Error('Token not valid');
 
-            return res.status(409).json( {error: error.message} );
+            return res.status(409).json({ error: error.message });
         }
 
         res.json('Token is valid');
-        
+
     }
 
     static resetPasswordWithToken = async (req: Request, res: Response) => {
 
-        const {token} = req.params;
+        const { token } = req.params;
 
         const { password } = req.body;
 
-        const user = await User.findOne({where: {token}})
-        if(!user) {
+        const user = await User.findOne({ where: { token } })
+        if (!user) {
             const error = new Error('Token not valid');
 
-            return res.status(404).json( {error: error.message} );
+            return res.status(404).json({ error: error.message });
         }
 
         user.password = await hashPassword(password);
@@ -153,6 +152,10 @@ export class AuthController {
         await user.save();
 
         res.json('Password reset successfully');
+    }
+
+    static user = async (req: Request, res: Response) => {
+        res.json(req.user);
     }
 
 }
--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -3,6 +3,7 @@ import { AuthController } from '../controllers/AuthController'
 import { body, param } from "express-validator";
 import { handleInputErrors } from '../middleware/validation';
 import { limiter } from "../config/limiter";
+import { authenticate } from "../middleware/suth";
 
 const router = Router();
 
@@ -62,5 +63,10 @@ router.post('/reset-password/:token',
         handleInputErrors,
         AuthController.resetPasswordWithToken
 );
+
+router.get('/user',
+    authenticate,
+    AuthController.user
+)
 
 export default router;
